ea387b71f936a5e6d49264487cd53ec90ead84b4
Phronima6 <160320302+Phronima6@users.noreply.github.com>
2024-08-11 20:57:33 +0300

    Функциональность «Рекомендации» (#9)
    * Функциональность «Рекомендации»
    
    * Привести в соответствие с замечаниями:
    - в классе UserServiceImpl, в методе getRecommendations добавить хранение списков понравившихся пользователю фильмов в List<Film> для избежания повторных обращений к базе данных и экономии ресурсов;
    - убрать лишние комментарии для лучшей читаемости кода.
    
    ---------
    
    Co-authored-by: Shestakov A. <schav91@gmail.com>

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.service.user;
 
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.List;
@@ -21,4 +22,8 @@ public interface UserService {
     void deleteFriend(Integer id,Integer friendId);
 
     List<User> getCommonFriends(Integer id, Integer otherId);
+
+    // Функциональность «Рекомендации»
+    List<Film> getRecommendations(Integer userId);
+
 }
