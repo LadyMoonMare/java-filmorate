590323dc5a1d56674d58e78149bc73f68b1917f8
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-08-12 00:29:37 +0300

    fix checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -6,8 +6,6 @@ import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
 
 @Data
 @Builder
