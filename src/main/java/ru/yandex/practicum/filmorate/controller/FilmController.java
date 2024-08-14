dcace9f1fe661dbad1d5a105c1adaa36bb26aff2
manager.shambala <lachins@mail.ru>
2024-08-14 03:19:53 +0200

    Запрос популярных фильмов 6.3

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -126,13 +127,10 @@ public class FilmController {
     }
 
     @Validated
-    @GetMapping("/popular")
-    public Collection<Film> getPopularFilms(
-            @RequestParam(name = "count",
-                    defaultValue = "10", required = false) Integer count,
-            @RequestParam(name = "genreId", required = false) Integer genreId,
-            @RequestParam(name = "year", required = false) Integer year
-    ) {
+    @GetMapping("/popular?count={limit}&genreId={genreId}&year={year}")
+    public List<Film> getPopularFilms(@RequestParam(required = false) @Min(0) Integer count,
+                                            @RequestParam(required = false) Integer genreId,
+                                            @RequestParam(required = false) @Min(1895) Integer year) {
         log.info("Запрос на получение популярных фильмов");
         List<Film> films = filmService.getPopular(count, genreId, year);
         log.info("Отправлено {} фильмов", count);
