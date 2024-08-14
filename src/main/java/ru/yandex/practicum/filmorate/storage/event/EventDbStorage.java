b3584ba3e9fbc2783d8a84479119ce24c13bc43e
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-08-15 02:07:55 +0300

    add addEvent and getFeed methods

--- src/main/java/ru/yandex/practicum/filmorate/storage/event/EventDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/event/EventDbStorage.java
@@ -18,12 +18,31 @@ public class EventDbStorage implements EventStorage {
 
     @Override
     public void addEvent(Event event) {
+        Integer eventTypeId = getEventTypeId(event);
+        Integer operationId = getOperationId(event);
+        jo.update("INSERT INTO feed (entity_id, user_id, event_type_id, operation_id) " +
+                "VALUES (?,?,?,?)",
+                event.getEntityId(),
+                event.getUserId(),
+                eventTypeId,
+                operationId);
+    }
 
+    public Integer getEventTypeId (Event event) {
+        return jo.queryForObject("SELECT id FROM event_type WHERE event_name = ?;", Integer.class,
+                event.getEventType().toString());
+    }
+
+    public Integer getOperationId (Event event) {
+        return jo.queryForObject("SELECT id FROM operation WHERE operation_name = ?;", Integer.class,
+                event.getOperation().toString());
     }
 
     @Override
     public List<Event> getFeed(Integer userId) {
-        return null;
+        return jo.query("SELECT * FROM feed AS f JOIN event_type AS et ON f.event_type_id = " +
+                "et.id JOIN operation AS o ON f.operation_id = o.id WHERE user_id = ?;",
+                erm, userId);
     }
 
 }
